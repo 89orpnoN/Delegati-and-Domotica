e74fdde5155af237a7421c71d4affb0e92071630
89orpnoN <9698022@itisrossi.vi.it>
2024-01-13 11:56:43 +0100

    defined behaviour for sensori and dispositivi. started main

--- Delegati-and-Domotica/Dispositivi.cs
+++ Delegati-and-Domotica/Dispositivi.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace Delegati_and_Domotica
 {
-    internal class Dispositivi
+    internal class Vocale
     {
-        
+        void parla(string testo)
+        {
+            Console.WriteLine(testo);
+        }
+        private event SendValueEventHandler _comandi;
+        public void Comandi(int value)
+        {
+            _comandi(value);
+        }
+
+    }
+    internal class Phone
+    {
+        private bool InviaEmail()
+    }
+    internal class Watch
+    {
+
     }
 }
--- Delegati-and-Domotica/Sensore.cs
+++ Delegati-and-Domotica/Sensore.cs
@@ -8,17 +8,36 @@ namespace Delegati_and_Domotica
 {
     internal abstract class Sensore
     {
-        private float _valore;
-        protected bool LeggiDato(out float Variable)
+        protected string Unita;
+        protected float? _valore;
+        protected bool LeggiDato(out float Variable) // lancia errore
         {
-            Variable = _valore;
-            return true;
-        }
+            Variable = _valore ?? 0;
+            return (_valore != null);
+        } 
+
+        protected event SendValueEventHandler SendValue;
+        protected event SendStatusEventHandler SendStatus;
+
     }
 
     internal class LightSensor : Sensore
     {
+        //fai costruttore
+        LightSensor() 
+        {
+            Unita = "Lumen";
+            _valore = 46;
+        }
+    }
 
+    internal class TempSensor : Sensore
+    {
+        TempSensor()
+        {
+            Unita = "Celsius";
+            _valore = 140;
+        }
     }
 
 }
